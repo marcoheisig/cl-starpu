7c4043ea015889579b0fd88b17e71a7f3ad033dc
Marco Heisig <marco.heisig@fau.de>
2022-04-12 16:19:30 +0200

    Improve starpu-init.

--- code/starpu.lisp
+++ code/starpu.lisp
@@ -19,12 +19,43 @@
      (cffi:mem-ref minor :int)
      (cffi:mem-ref release :int))))
 
-(defun starpu-init (&key)
+(defun starpu-init
+    (&key ncpus reserve-ncpus ncuda nopencl nmic nmpi-ms bus-calibrate calibrate
+       single-combined-worker
+       disable-asynchronous-copy
+       disable-asynchronous-cuda-copy
+       disable-asynchronous-opencl-copy
+       disable-asynchronous-mic-copy)
   (unless (starpu-is-initialized)
     (cffi:with-foreign-object (conf '(:struct starpu-conf-cstruct))
       (%starpu-conf-init conf)
       ;; Ensure that StarPU doesn't override our signal handlers.
       (setf (cffi:foreign-slot-value conf '(:struct starpu-conf-cstruct) 'catch-signals-slot) 0)
+      ;; Process all keyword options.
+      (macrolet ((process-unsigned-byte-option (variable slot)
+                   `(etypecase ,variable
+                      (null)            ; Keep the default.
+                      (unsigned-byte
+                       (setf (cffi:foreign-slot-value conf '(:struct starpu-conf-cstruct) ',slot)
+                             ,variable))))
+                 (process-boolean-option (variable slot)
+                   `(progn
+                      (check-type ,variable boolean)
+                      (setf (cffi:foreign-slot-value conf '(:struct starpu-conf-cstruct) ',slot)
+                            (if ,variable 1 0)))))
+        (process-unsigned-byte-option ncpus ncpus-slot)
+        (process-unsigned-byte-option reserve-ncpus reserve-ncpus-slot)
+        (process-unsigned-byte-option ncuda ncuda-slot)
+        (process-unsigned-byte-option nopencl nopencl-slot)
+        (process-unsigned-byte-option nmic nmic-slot)
+        (process-unsigned-byte-option nmpi-ms nmpi-ms-slot)
+        (process-boolean-option bus-calibrate bus-calibrate-slot)
+        (process-boolean-option calibrate calibrate-slot)
+        (process-boolean-option single-combined-worker single-combined-worker-slot)
+        (process-boolean-option disable-asynchronous-copy disable-asynchronous-copy-slot)
+        (process-boolean-option disable-asynchronous-cuda-copy disable-asynchronous-cuda-copy-slot)
+        (process-boolean-option disable-asynchronous-opencl-copy disable-asynchronous-opencl-copy-slot)
+        (process-boolean-option disable-asynchronous-mic-copy disable-asynchronous-mic-copy-slot))
       ;; Actually initialize.
       (%starpu-init conf))))
 
@@ -52,7 +83,4 @@
     (let ((task (%starpu-task-create)))
       (setf (cffi:foreign-slot-value task '(:struct starpu-task-cstruct) 'cl-slot)
             cl)
-      #+(or)
-      (setf (cffi:foreign-slot-value task '(:struct starpu_task) 'synchronous)
-            1)
       (%starpu-task-submit task))))
